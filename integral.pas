b14842b758019e9f5da3221c6ecc24299eefd969
.öldurot <iflybkeir2000@mail.ru>
2026-08-24 01:34:16 +0300

    v0.9

--- integral.pas
+++ integral.pas
@@ -1,18 +1,18 @@
 ﻿Program integral;
 var delta_x := 0.01;
 var a := 0.0;
-var b:= 3/4;
-var x:double;
+var b:= 1.01;
+var x:=0.0;
 var summ_integral:=0.0;
 
 begin
   PrintLn('I','  integral',' I', '     x','    I'); 
-  while x < b do
+  while x <= b do
   begin
     summ_integral := summ_integral + (cos(x) * delta_x);
-    x := x + delta_x;
-    PrintLn('I','------------------------','I'); 
+    PrintLn('I','------------------------','I');
     PrintLn('I ', summ_integral:0:8,'I   ',x:0:2,'   I');
+    x := x + delta_x;
   end;
   PrintLn('I','------------------------','I'); 
 end.
